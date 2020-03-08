23cf0fb085a917208b3ccccbd3f19eb66eec0c66
Diogo Gomes <diogo.gomes77@gmail.com>
2020-03-08 15:14:40 +0000

    added ProjectParticipationIssue model

--- kww_project/kww_app/models.py
+++ kww_project/kww_app/models.py
@@ -82,3 +82,10 @@ class TechnologyUse(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
+class ProjectParticipationIssue(models.Model):
+    participation = models.ForeignKey('ProjectParticipation', null=False, related_name="issues", on_delete=models.CASCADE)
+    issue = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
