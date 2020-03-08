932d8ceb1398a95cf6a84a1341117e5f7ff81d20
Diogo Gomes <diogo.gomes77@gmail.com>
2020-03-08 15:12:58 +0000

    added date fields to Tecnology and TecnologyUse

--- kww_project/kww_app/models.py
+++ kww_project/kww_app/models.py
@@ -66,6 +66,9 @@ class Technology(models.Model):
     name = models.CharField(max_length=64)
     parent = models.ForeignKey('Technology', null=True, blank=True, on_delete=models.CASCADE)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.name
 
@@ -74,5 +77,8 @@ class TechnologyUse(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
     technology = models.ForeignKey(Technology, on_delete=models.CASCADE, null=True)
     participant = models.ForeignKey(Participant, on_delete=models.CASCADE, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
