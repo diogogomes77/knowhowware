0312620f6f494b30551049a8c23f11b6c46236aa
Diogo Gomes <diogo.gomes77@gmail.com>
2020-03-22 10:10:12 +0000

    added function for lateral menu options

--- kww_app/views.py
+++ kww_app/views.py
@@ -4,23 +4,34 @@ from django.views.generic import TemplateView, ListView
 from kww_app.models import Project
 
 
+def get_lateral_menu_options():
+    pages = []
+    projects = {
+        'name': "Projects",
+        'href': "/projects"
+    }
+    pages.append(projects)
+    return pages
+
+
 class HomeView(TemplateView):
     template_name = 'kww_app/index.html'
 
     def get_context_data(self, *args, **kwargs):
         context = super(HomeView, self).get_context_data(*args, **kwargs)
-        pages = []
-        projects = {
-            'name': "Projects",
-            'href': "/projects"
-        }
-        pages.append(projects)
-        context['pages'] = pages
+        context['pages'] = get_lateral_menu_options()
         return context
 
 
-class ProjectsView(HomeView):
+class ProjectsView(ListView):
+    model = Project
     template_name = 'kww_app/project_list.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProjectsView, self).get_context_data(*args, **kwargs)
+        context['pages'] = get_lateral_menu_options()
+        return context
+
 
 
 
